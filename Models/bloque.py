4019578ee365b37c539dd3e236c843ea89783b3a
José Astorga <jose.n.astorga.tobar@gmail.com>
2018-05-10 01:15:07 -0300

    add bloque model (basic)

---
+++ Models/bloque.py
@@ -0,0 +1,49 @@
+import os
+import random
+from Utils.CC3501Utils import *
+
+####################################################
+# Clase Bloque
+####################################################
+
+class Bloque(Figura):
+    def __init__(self, pos=Vector(0, 0), rgb=(1.0, 1.0, 1.0)):
+
+        self.divs = []
+        for i in range(0,5):
+            self.divs.append(random.randint(10,40))
+
+        super().__init__(pos, rgb)
+
+    def figura(self):
+        # Se dibuja la nube
+
+        glBegin(GL_QUADS)
+
+        glColor3f(130.0/255.0, 130.0/255.0, 130.0/255.0) 
+
+        # Rectangulo principal
+        
+        for i in range(0,5):
+
+            glVertex2f(49,i*10+1)
+            glVertex2f(1,i*10+1)
+            glVertex2f(1,(i+1)*10-1)
+            glVertex2f(49,(i+1)*10-1)
+
+        glEnd()
+
+
+        glBegin(GL_LINES)
+        
+        glColor3f(90.0/255.0, 90.0/255.0, 90.0/255.0) 
+        
+        for i in range(0,5):
+            glVertex2f(47,i*10)
+            glVertex2f(3,i*10)
+
+            glVertex2f(self.divs[i], i*10)
+            glVertex2f(self.divs[i], (i+1)*10)
+
+
+        glEnd()
