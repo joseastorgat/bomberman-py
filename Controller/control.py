b3b3bb1edeecef5a823c74fbd65673c560a981b7
José Astorga <jose.n.astorga.tobar@gmail.com>
2018-05-10 01:17:46 -0300

    add movement and bombs to enemys >:) :cat:

--- Controller/control.py
+++ Controller/control.py
@@ -11,6 +11,7 @@ from Models.fondo    import Fondo
 from Models.ladrillo import Ladrillo
 from Models.bomber   import Bomber
 from Models.bomba    import Bomba
+from Models.bloque   import Bloque
 
 class Controller:
     def __init__(self, width, height, scale = 50):
@@ -32,23 +33,31 @@ class Controller:
 
         self.fondo = Fondo()
         
-        pos_ladrillos = borde_ladrillo(width,height)
+        pos_ladrillos = get_ladrillos_pos(width,height)
         
         self.ladrillos = []
         for pos in pos_ladrillos:
             self.ladrillos.append(Ladrillo(pos))
 
+        pos_bloques = get_bloques_pos(width,height)
+
+        self.bloques = []
+        for pos in pos_bloques:
+            self.bloques.append(Bloque(pos))
+
         self.bomber = Bomber(Vector(1*scale,1*scale))
         
         self.bombas = []
-
 
         self.enemigos = [Bomber(Vector(width-2*scale, 1*scale)), 
                          Bomber(Vector(width-2*scale, height-2*scale)), 
                          Bomber(Vector(1*scale, height-2*scale))]
 
+        self.enemigos[0].set_vel(Vector(-1,0))
+        self.enemigos[1].set_vel(Vector(0,-1))
+        self.enemigos[2].set_vel(Vector(1,0))
 
-        self.vista = vista.Vista(self.fondo, self.ladrillos, self.bomber,  self.bombas, self.enemigos)
+        self.vista = vista.Vista(self.fondo, self.ladrillos, self.bomber,  self.bombas, self.enemigos, self.bloques)
         self.run = True
     
 
@@ -58,17 +67,19 @@ class Controller:
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
-            self.bomber.move(Vector(1,0))
+            self.bomber.set_vel(Vector(1,0))
 
         elif keys[pygame.K_LEFT]:
-            self.bomber.move(Vector(-1,0))
+            self.bomber.set_vel(Vector(-1,0))
 
         elif keys[pygame.K_UP]:
-            self.bomber.move(Vector(0,1))
+            self.bomber.set_vel(Vector(0,1))
 
         elif keys[pygame.K_DOWN]:
-            self.bomber.move(Vector(0,-1))
+            self.bomber.set_vel(Vector(0,-1))
 
+        else:
+            self.bomber.set_vel(Vector(0,0))
 
         for event in pygame.event.get():
 
@@ -91,8 +102,29 @@ class Controller:
 
         #Update Actual scenario
         self.bomber.explode_bombs()
+        self.bomber.move()
 
+        for enemigo in self.enemigos:
+            
+            des = np.random.choice(np.arange(0, 4), p=[0.8,0.075,0.075,0.05])
+            if des == 0:
+                pass
+            
+            elif des == 1:
+                enemigo.set_vel(Vector(enemigo.vel.y, enemigo.vel.x))
 
+            elif des == 2:
+                enemigo.set_vel(Vector(-enemigo.vel.y, -enemigo.vel.x))
+
+            elif des == 4:
+                enemigo.set_vel(Vector(-enemigo.vel.x, -enemigo.vel.y))
+
+            enemigo.move()
+
+            bomb = np.random.choice(np.arange(0, 2), p=[0.95,0.05])
+            if bomb == 1:
+                enemigo.release_bomb()
+            enemigo.explode_bombs()
 
 
         #Action for Enemy Bombers
@@ -113,7 +145,7 @@ class Controller:
             #pos ladrillos destructibles
 
 
-def borde_ladrillo(width,height):
+def get_ladrillos_pos(width,height):
 
     pos_ladrillos = []
     for i in range(int(height/50)):
@@ -130,6 +162,18 @@ def borde_ladrillo(width,height):
                 pos_ladrillos.append(Vector(50*i, 50*j))
 
     return pos_ladrillos
+
+def get_bloques_pos(width,height):
+
+    pos_bloques = []
+
+    for i in range(1,int(width/50)-1):
+        for j in range(1,int(height/50)-1):
+            if i%2!=0 and j%2!=0:
+                pos_bloques.append(Vector(50*i, 50*j))
+    return pos_bloques
+
+
 
 
 def generate_initial_map(width,height,scale):
--- View/vista.py
+++ View/vista.py
@@ -2,12 +2,13 @@ from OpenGL.GL import *
 from Utils.CC3501Utils import *
 
 class Vista:
-    def __init__(self, fondo, ladrillos, bomber, bombas, enemigos):
+    def __init__(self, fondo, ladrillos, bomber, bombas, enemigos, bloques):
         self.fondo     = fondo
         self.bomber    = bomber
         self.bombas    = bombas
         self.enemigos  = enemigos
         self.ladrillos = ladrillos
+        self.bloques   = bloques
 
     def dibujar(self):
         # revisar si es necesario volver a negro la pantalla
@@ -17,12 +18,18 @@ class Vista:
         for ladrillo in self.ladrillos:
             ladrillo.dibujar()
         
-        for bomba in self.bomber.active_bombs:
-            bomba.dibujar()
+        for bloque in self.bloques:
+            bloque.dibujar()
 
         for enemigo in self.enemigos:
+            for bomba_enemiga in enemigo.active_bombs:
+                bomba_enemiga.dibujar()
+        
             enemigo.dibujar()
         
         self.bomber.dibujar()
+        
+        for bomba in self.bomber.active_bombs:
+            bomba.dibujar()
         
         pygame.display.flip()
